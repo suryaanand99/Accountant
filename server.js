7509882068cd14eb00bace08944746bad6db4f71
Surya Anand <anandsurya1989@gmail.com>
2020-03-25 18:07:25 +0530

    dummy

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const port = process.env.PORT;
+
+const app = express();
+
+app.get("/home", (req, res) => {
+  console.log("hi da!");
+  res.send();
+});
+
+app.listen(port, () => {
+  console.log("server running successfully");
+});
